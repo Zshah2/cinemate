a072d75be64bd14a07b11a9f5a3cbd29a3b19e45
Mohammad <mohammad@Mohammads-MacBook-Air.local>
2026-04-01 13:57:54 -0400

    moved pip statement

--- start.sh
+++ start.sh
@@ -6,9 +6,10 @@ CSV="IMDB Movies 2000 - 2020.csv"
 
 if [ ! -d venv ]; then 
     python3 -m venv venv
-    pip install -r requirements.txt
 fi
 source venv/bin/activate
+pip install -r requirements.txt
+
 
 if [ ! -f "$CSV" ]; then
     python download_data.py
