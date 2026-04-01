040161e0b500503debce5a5a2c45d9ea858c4ce8
Mohammad <mohammad@Mohammads-MacBook-Air.local>
2026-04-01 13:50:13 -0400

    bugfixes

--- start.sh
+++ start.sh
@@ -2,15 +2,15 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-CSV = "IMDB Movies 2000 - 2020.csv"
+CSV="IMDB Movies 2000 - 2020.csv"
 
-if [! -d venv] then 
+if [ ! -d venv ]; then 
     python3 -m venv venv
     pip install -r requirements.txt
 fi
 source venv/bin/activate
 
-if [! -f $CSV] then
+if [ ! -f $CSV]; then
     python download_data.py
 fi
 
